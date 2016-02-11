9e4723a388e466a13bf5c7bbac2706aea74a747d
MrHidalgo <Luckyhidalgo@gmail.com>
2016-02-11 20:13:00 +0200

    v0.1 - gulp template(modify stuct files): - new packages; - new str files project; - modify gulpfile;

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,126 @@
 'use strict'
 
 /* NPM PACKAGES
-=================================*/
+ =================================*/
 var gulp            =   require('gulp'),
-    mainBowerFiles  =   require('main-bower-files'),    // MAIN BOWER FILES
-    watch           =   require('gulp-watch'),          // WATCH FILE
+    mainBowerFiles  =   require('main-bower-files'),    // MAIN BOWER FILES (**.min.**)
+    watch           =   require('gulp-watch'),          // WATCH FILE CHANGED
     browserSync     =   require('browser-sync'),        // LIVERELOAD & SERVER PROJECT
-    rimraf          =   require('rimraf'),              // CLEAN PATH
     util            =   require('gulp-util'),           // FOR WATCH ERROR
-    plumber         =   require('gulp-plumber'),        // FOR WATCH ERROR
-    notify          =   require("gulp-notify"),         // NOTIFY
+    plumber         =   require('gulp-plumber'),        // ERROR TRAPPING REALTIME
+    jshint          =   require('gulp-jshint'),         // JS HINT
+    del             =   require('del'),                 // CLEAN [FOLDER, FILES]
+    jade            =   require('gulp-jade'),           // JADE TEMPLATE
+    scss            =   require('gulp-sass'),           // SASS
+    prefixer        =   require('gulp-autoprefixer'),   // AUTOPREFIXER
+    sourcemaps      =   require('gulp-sourcemaps'),     // SOURCEMAPS
+    prettify        =   require('gulp-prettify'),       // REFORMAT CODE
+    cssmin          =   require('gulp-minify-css'),     // MINIFY CSS FILE
+    jsmin           =   require('gulp-uglifyjs'),       // MINIFY JS FILE
+    rename          =   require('gulp-rename'),         // RENAME FILES
+    lazypipe        =   require('lazypipe'),            // PARTIAL PIPELINES
     reload          =   browserSync.reload;             // RELOAD
 
 
+/* PATH FOR FILES
+ =================================*/
+var path = {
+
+    // FINISH FILE PROJECT
+    dist: {
+        //
+        jade    :   './dist/',
+        scss    :   './dist/style/',
+        font    :   './dist/style/',
+        script  :   './dist/script/'
+    },
+
+    // WORK FILES
+    src: {
+        //
+        jade    :   './src/index.jade',
+        scss    :   './src/sassTemplate/**.scss',
+        font    :   './src/sassTemplate/_font/**.scss',
+        script  :   './src/script/**.js'
+    },
+
+    // STREAM/WATCH FILE
+    watch:{
+        //
+        jade        :   './src/**.jade',
+        jadeWatch   :   './src/**/**.jade',
+        scss        :   './src/sassTemplate/**.scss',
+        scssWatch   :   './src/sassTemplate/**/**.scss',
+        font        :   './src/sassTemplate/_font/**.scss',
+        script      :   './src/script/**.js'
+    },
+
+    // CLEAN FOLDER
+    clean       :   './dist/*',
+    cleanScript :   './dist/script/*',
+    cleanStyle  :   './dist/style/*',
+    cleanHtml   :   './dist/*.html'
+};
+
+
+/* TEXT EXAMPLE
+ =================================*/
+var textExample = {
+
+    //SERVER
+    server          :   'server',
+    tunnel          :   'tunnel',
+
+    //WATCH FILE
+    watch           :   'watch',
+
+    //BUILD PROJECT FILES
+    build           :   'build',
+    buildMain       :   'main',
+    buildJade       :   'build:jade',
+    buildScss       :   'build:scss',
+    buildFont       :   'build:font',
+    buildScript     :   'build:script',
+    buildMainFiles  :   'mainFiles',
+
+    //CLEAN PROJECT FILES
+    cleanGlobal     :   'clean:global',
+    cleanScript     :   'clean:script',
+    cleanStyle      :   'clean:style',
+    cleanHtml       :   'clean:html',
+    cleanProject    :   'clean:project',
+
+    //MAIN BOWER FILES
+    bowerJquery     :   'main:jquery',
+    bowerJqueryMin  :   './dist/jquery.min.js',
+    bowerJqueryPath :   './dist/script',
+
+    //RENAME FILES
+    renameScript    :   'script.min.js',
+    renameStyle     :   'style.min.css',
+
+    //ERROR
+    error           :   'error'
+};
+
+
+/* SERVER CONFIG
+ =================================*/
+var config = {
+    server: {
+        baseDir :   './dist'
+    },
+    tunnel      :   'lucly',
+    online      :   true,
+    notify      :   true,
+    host        :   'localhost',
+    port        :   1234,
+    logPrefix   :   'FrontEnd Server'
+};
+
+
 /* ERROR
-=================================*/
+ =================================*/
 var reportError = function(error) {
     var lineNumber  = (error.lineNumber) ? 'LINE ' + error.lineNumber + ' -- ' : '',
         report      = '',
@@ -33,182 +139,288 @@ var reportError = function(error) {
     if (error.lineNumber) {
         report +=   chalk('LINE: ') + ' ' + error.lineNumber + '\n';
     }
-
     if (error.fileName) {
         report +=   chalk('FILE: ') + ' ' + error.fileName + '\n';
     }
 
     console.error(report);
-
     this.emit('end');
-}
-
-
-/* PATH FOR FILES
-=================================*/
-var path = {
-    // Finish files project
-    dist: {
-        //
-    },
-
-    // Work with files
-    src: {
-        //
-    },
-
-    // Watch all time
-    watch:{
-        //
-    },
-
-    // clean path
-    clean: './dist/*'
 };
 
 
-/* SERVER CONFIG
-=================================*/
-var config = {
-    server: {
-        baseDir : './dist'
-    },
-    tunnel      : true,
-    host        : 'localhost',
-    port        : 9000,
-    logPrefix   : 'FrontEnd Server'
-};
+/*
+ TEMPLATE BLOCK [notify, reload]
+ ==============================*/
+var reloadTemplate = lazypipe()
+    .pipe( function() {
+        return reload(
+            {
+                stream: true
+            }
+        );
+    })
+
+
+/*
+ TEMPLATE BLOCK SASS
+ ==============================*/
+var optionsScssTemplate = lazypipe()
+    .pipe( function() {
+        return scss(
+            {
+                sourceMap        : true,
+                errLogToConsole  : true,
+                outputStyle      : 'compressed'
+            }
+        );
+    })
 
 
 /* WEB-SERVER
-=================================*/
-gulp.task('server', function() {
+ =================================*/
+gulp.task(textExample.server, function() {
     browserSync(config);
 });
 
 
-/* CLEAN PROJECT
-=================================*/
-gulp.task('clean', function(e) {
-    rimraf(path.clean, e);
+/* CLEAN FOLDER PROJECT
+ =================================*/
+gulp.task(textExample.cleanGlobal, function() {
+    del.sync(path.clean);
+});
+gulp.task(textExample.cleanHtml, function() {
+    del.sync(path.cleanHtml);
+});
+gulp.task(textExample.cleanStyle, function() {
+    del.sync(path.cleanStyle);
+});
+gulp.task(textExample.cleanScript, function() {
+    del.sync(path.cleanScript);
 });
 
 
 /* WATCH FILES FOR RELOAD & SYNC
-=================================*/
-gulp.task('watch', function(){
-    //watch([path.watch.jade], function() {
-    //    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    //    gulp.start();
-    //});
-    //watch([path.watch.html], function() {
-    //    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    //    gulp.start();
-    //});
-    //watch([path.watch.scss], function() {
-    //    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    //    gulp.start();
-    //});
+ =================================*/
+gulp.task(textExample.watch, function(){
+    watch(
+        [
+            path.watch.jade,
+            path.watch.jadeWatch,
+            path.watch.scss,
+            path.watch.scssWatch,
+            path.watch.font,
+            path.watch.script
+        ],
+        function() {
+            gulp.start(textExample.build);
+        });
 });
 
 
-/* MAIN FILES
+/* MAIN BOWER FILES
  =================================*/
-gulp.task('bowerMain', function() {
+gulp.task(textExample.bowerJquery, function() {
     var main = gulp.src(mainBowerFiles(
         {
             overrides: {
                 jquery : {
                     main : [
-                        './dist/jquery.min.js'
+                        textExample.bowerJqueryMin
                     ]
                 }
             }
         }
         ))
-        .pipe(gulp.dest('./dist/script'));
+        .pipe(gulp.dest(textExample.bowerJqueryPath))
 
     return main;
 });
 
 
 /*
+ SCRIPT build
+ ==============================*/
+gulp.task(textExample.buildScript, function () {
+    gulp
+        .src(
+            path.src.script
+        )
+        .pipe(jshint())
+        .pipe(plumber(
+            {
+                errorHundler : reportError
+            }
+        ))
+        .pipe(
+            jsmin()
+        )
+        .pipe(
+            rename(textExample.renameScript)
+        )
+        .pipe(
+            reloadTemplate()
+        )
+        .pipe(
+            gulp.dest(
+                path.dist.script
+            )
+        )
+        .on(textExample.error, reportError)
+});
+
+/*
+ SCSS FONT build
+ ==============================*/
+gulp.task(textExample.buildFont, function() {
+    gulp
+        .src(
+            path.src.font
+        )
+        .pipe(plumber(
+            {
+                errorHundler : reportError
+            }
+        ))
+        .pipe(
+            sourcemaps.init()
+        )
+        .pipe(
+            optionsScssTemplate()
+        )
+        .pipe(
+            sourcemaps.write()
+        )
+        .pipe(
+            reloadTemplate()
+        )
+        .pipe(
+            gulp.dest(
+                path.dist.font
+            )
+        )
+        .on(textExample.error, reportError)
+});
+
+
+/*
+ SCSS FILES build
+ ==============================*/
+gulp.task(textExample.buildScss, function() {
+    gulp
+        .src(
+            path.src.scss
+        )
+        .pipe(plumber(
+            {
+                errorHundler : reportError
+            }
+        ))
+        .pipe(
+            sourcemaps.init()
+        )
+        .pipe(
+            optionsScssTemplate()
+        )
+        .pipe(
+            prefixer(
+                {
+                    browsers         : ['last 3 versions'],
+                    cascade          : true
+                }
+            ))
+        .pipe(
+            sourcemaps.write()
+        )
+        .pipe(
+            cssmin(
+                {
+                    compatibility    : 'ie9'
+                }
+            ))
+        .pipe(
+            rename(textExample.renameStyle)
+        )
+        .pipe(
+            reloadTemplate()
+        )
+        .pipe(
+            gulp.dest(
+                path.dist.scss
+            )
+        )
+        .on(textExample.error, reportError)
+});
+
+
+/*
+ BUILD JADE TEMPLATE
+ ==============================*/
+gulp.task(textExample.buildJade, function() {
+    var YOUR_LOCALS = {};
+
+    gulp
+        .src(
+            path.src.jade
+        )
+        .pipe(plumber(
+            {
+                errorHundler : reportError
+            }
+        ))
+        .pipe(
+            jade(
+                {
+                    locals      :   YOUR_LOCALS
+                }
+            ))
+        .pipe(
+            prettify(
+                {
+                    indent_size :   4
+                }
+            ))
+        .pipe(
+            reloadTemplate()
+        )
+        .pipe(
+            gulp.dest(
+                path.dist.jade
+            )
+        )
+        .on(textExample.error, reportError)
+});
+
+
+/*
  TASK FOR MAIN FILES
  ==============================*/
-gulp.task('mainFiles',
+gulp.task(textExample.buildMainFiles,
     [
         //JQUERY
-        'bowerMain'
+        textExample.bowerJquery
     ]
 );
 
 
 /*
-  SCSS FILES build
-==============================*/
-//gulp.task('build:mainSCSS', function() {
-//    gulp.src(path.src.scss)
-//        .pipe(plumber(
-//            {
-//                errorHundler         : reportError
-//            }
-//        ))
-//        .pipe(sourcemaps.init())
-//        .pipe(sass(
-//            {
-//                sourceMap        : true,
-//                errLogToConsole  : true,
-//                outputStyle      : 'compressed'
-//            }
-//        ))
-//        .pipe(sourcemaps.write())
-//        .pipe(prefixer(
-//            {
-//                browsers         : ['last 3 versions'],
-//                cascade          : true
-//            }
-//        ))
-//        .pipe(cssmin(
-//            {
-//                compatibility    : 'ie9'
-//            }
-//        ))
-//        .pipe(gulp.dest(path.dist.scss))
-//        .pipe(reload(
-//            {
-//                stream: true
-//            }
-//        ))
-//        .pipe(notify(
-//            {
-//                message          : "MAIN SASS FILE CREATE : <%= file.relative %> @ <%= options.date %>",
-//                templateOptions  : {
-//                    date: new Date()
-//                }
-//            }
-//        ))
-//        .on('error', reportError)
-//});
-
-
-/*
  MAIN BUILD "ALL TASK"
-==============================*/
-//gulp.task('build',
-//    [
-//        'build:jade',
-//        'build:mainHTML',
-//        'build:mainSCSS'
-//    ]
-//);
+ ==============================*/
+gulp.task(textExample.build,
+    [
+        textExample.buildJade,
+        textExample.buildScss,
+        textExample.buildFont,
+        textExample.buildScript
+    ]
+);
 
 
 /* MAIN TASK FOR PROJECT
-=================================*/
-gulp.task('main',
+ =================================*/
+gulp.task(textExample.buildMain,
     [
-        'build',
-        'server',
-        'watch'
+        textExample.build,
+        textExample.server,
+        textExample.watch
     ]
 );
